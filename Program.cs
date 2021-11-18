5e7a5aafc30088d34e327f1e659c496ec9a0d67f
riqvade <40538535+riqvade@users.noreply.github.com>
2021-11-18 13:50:33 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VectorClassGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector[] vectors = Vector.CreateVectors(2, 0, 100);
+
+            Console.WriteLine($"Вектор 1: {vectors[0]}");
+            Console.WriteLine($"Вектор 2: {vectors[1]}");
+            Console.WriteLine($"Длина вектора 1: {vectors[0].Length()}");
+            Console.WriteLine($"Длина вектора 2: {vectors[1].Length()}");
+
+            Console.WriteLine($"Скалярное произведение двух векторов: {vectors[0].ScalarProduct(vectors[1])}");
+            Console.WriteLine($"Произведение векторов: {vectors[0].СrossProductWithDifferentVector(vectors[1])}");
+            Console.WriteLine($"Угол между векторами: {vectors[0].Cos(vectors[1])}");
+            Console.WriteLine($"Сумма векторов: {vectors[0].Sum(vectors[1])}");
+            Console.WriteLine($"Разность векторов: {vectors[0].Difference(vectors[1])}");
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace VectorClassGenerator
+{
+    internal class Vector
+    {
+        public Vector(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        private double X { get; }
+        private double Y { get; }
+        private double Z { get; }
+
+
+        /// <summary>
+        /// Возвращает длину вектора
+        /// </summary>
+        public double Length()
+        {
+            int degree = 2;
+
+            return Math.Sqrt(Math.Pow(X, degree) + Math.Pow(Y, degree) + Math.Pow(Z, degree));
+        }
+
+        /// <summary>
+        /// Возвращает скалярное произведение двух векторов
+        /// </summary>
+        public double ScalarProduct(Vector vector)
+        {
+            return X * vector.X + Y * vector.Y + Z * vector.Z;
+        }
+
+        /// <summary>
+        /// Возвращает векторное произведение с другим вектором
+        /// </summary>
+        public Vector СrossProductWithDifferentVector(Vector vector)
+        {
+            return new Vector(Y * vector.Z - Z * vector.Y, Z * vector.X - X * vector.Z, X * vector.Y - Y * vector.X);
+        }
+
+        /// <summary>
+        /// Возвращает угол между векторами
+        /// </summary>
+        public double Cos(Vector vector)
+        {
+            return ScalarProduct(vector) / (Length() * vector.Length());
+        }
+
+        /// <summary>
+        /// Возвращает сумму векторов
+        /// </summary>
+        public Vector Sum(Vector vector)
+        {
+            return new Vector(X + vector.X, Y + vector.Y, Z + vector.Z);
+        }
+
+        /// <summary>
+        /// Возвращает разность векторов
+        /// </summary>
+        public Vector Difference(Vector vector)
+        {
+            return new Vector(X - vector.X, Y - vector.Y, Z - vector.Z);
+        }
+
+        /// <summary>
+        /// Возвращает массив векторов.
+        /// </summary>
+        /// <param name="number">Колличество генерируемых векторов.</param>
+        /// <param name="minRandomСoordinateValue">Минимальное генерируемое значение координаты вектора.</param>
+        /// <param name="maxRandomСoordinateValue">Максимальное генерируемое значение координаты вектора.</param>
+        public static Vector[] CreateVectors(int number, int minRandomСoordinateValue, int maxRandomСoordinateValue)
+        {
+            Random random = new Random();
+            Vector[] vectors = new Vector[number];
+            for(int i = 0; i < number; i++)
+            {
+                vectors[i] = new Vector(random.Next(minRandomСoordinateValue, maxRandomСoordinateValue),
+                    random.Next(minRandomСoordinateValue, maxRandomСoordinateValue),
+                    random.Next(minRandomСoordinateValue, maxRandomСoordinateValue));
+            }
+
+            return vectors;
+        }
+        
+        public override string ToString()
+        {
+            return $"Vector: x = {X}, y = {Y}, z = {Z}";
+        }
+    }
+}
